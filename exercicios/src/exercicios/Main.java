154d189fdf2c4609bde3d48a159aea5989fde354
UserPL135Pc02 <UserPL135Pc02@SLFRPL135PC02.ensino.atec.local>
2016-12-21 08:26:12 +0000

    exercicio6 done

--- exercicios/src/exercicios/Main.java
+++ exercicios/src/exercicios/Main.java
@@ -1,11 +1,12 @@
 package exercicios;
 
 import nivel3.*;
+import nivel2.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new exercicio10();
+		new exercicio6();
 	}
 
 }
--- exercicios/src/nivel2/exercicio6.java
+++ exercicios/src/nivel2/exercicio6.java
@@ -3,8 +3,19 @@ package nivel2;
 import tools.easyIO;
 
 public class exercicio6 {
+	
+	static easyIO c = new easyIO();
+	
 	public exercicio6() {
 		int var1, var2;
-		
+		c.writeLine("Introduza dois números");
+		var1 = c.readN();
+		var2 = c.readN();
+		c.writeLine("Vão aparecer todos os números pares entre os números introduzidos");
+		for (int i=var1; i<=var2; i++) {
+			if (i%2==0) {
+				c.writeLine(i + " ");
+			}
+		}
 	}
 }
